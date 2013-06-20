21b928e604957099e6d91b0c2b972119ff674343
jcaddel <jcaddel@kuali.org>
2013-06-20 15:33:50 -0400

    tmp area
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-enroll/tags/builds/ks-enroll-1.0/1.0.0/build-261@50184 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class1/krms/service/impl/RuleStudentViewHelperServiceImpl.java
@@ -0,0 +1,266 @@
+package org.kuali.student.enrollment.class1.krms.service.impl;
+
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.krad.uif.service.impl.ViewHelperServiceImpl;
+import org.kuali.rice.krms.api.repository.language.NaturalLanguageTemplate;
+import org.kuali.rice.krms.api.repository.language.NaturalLanguageUsage;
+import org.kuali.rice.krms.impl.repository.NaturalLanguageTemplateBoService;
+import org.kuali.rice.krms.impl.repository.NaturalLanguageUsageBoService;
+import org.kuali.student.enrollment.class1.krms.dto.KrmsSuggestDisplay;
+import org.kuali.student.enrollment.class1.krms.service.RuleStudentViewHelperService;
+import org.kuali.student.enrollment.class1.krms.service.TemplateResolverService;
+import org.kuali.student.enrollment.class1.krms.util.KsKrmsConstants;
+import org.kuali.student.enrollment.class1.krms.util.KsKrmsRepositoryServiceLocator;
+import org.kuali.student.enrollment.class2.courseoffering.service.decorators.PermissionServiceConstants;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.util.ContextUtils;
+import org.kuali.student.r2.common.util.constants.OrganizationServiceConstants;
+import org.kuali.student.r2.core.organization.service.OrganizationService;
+import org.kuali.student.r2.core.search.dto.SearchParamInfo;
+import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultCellInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultInfo;
+import org.kuali.student.r2.core.search.dto.SearchResultRowInfo;
+import org.kuali.student.r2.lum.clu.service.CluService;
+import org.kuali.student.r2.lum.util.constants.CluServiceConstants;
+
+import javax.xml.namespace.QName;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: SW
+ * Date: 2012/12/04
+ * Time: 11:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RuleStudentViewHelperServiceImpl extends ViewHelperServiceImpl implements RuleStudentViewHelperService{
+
+    private CluService cluService;
+    private ContextInfo contextInfo;
+    private OrganizationService organizationService;
+
+    @Override
+    public String getTermSpecificationForType(String type) {
+        return KsKrmsRepositoryServiceLocator.getTemplateResolverService().getTermSpecificationForType(type);
+    }
+
+    @Override
+    public String getOperationForType(String type) {
+        return KsKrmsRepositoryServiceLocator.getTemplateResolverService().getOperationForType(type);
+    }
+
+    @Override
+    public String getValueForType(String type) {
+        return KsKrmsRepositoryServiceLocator.getTemplateResolverService().getValueForType(type);
+    }
+
+    public List<KrmsSuggestDisplay> getCourseNamesForSuggest(String moduleName) {
+
+        List<KrmsSuggestDisplay> displays = new ArrayList<KrmsSuggestDisplay>();
+        List<SearchParamInfo> queryParamValueList = new ArrayList<SearchParamInfo>();
+        SearchParamInfo stateKeyParam = new SearchParamInfo();
+        stateKeyParam.setKey("lu.queryParam.luOptionalState");
+
+        List<String> stateValues = new ArrayList<String>();
+        stateValues.add("Active");
+        stateValues.add("Approved");
+        stateKeyParam.setValues(stateValues);
+        queryParamValueList.add(stateKeyParam);
+        SearchParamInfo cluCodeParam = new SearchParamInfo();
+        cluCodeParam.setKey("lu.queryParam.luOptionalCode");
+        cluCodeParam.getValues().add(moduleName);
+        queryParamValueList.add(cluCodeParam);
+
+        SearchRequestInfo searchRequest = new SearchRequestInfo();
+        searchRequest.setSearchKey("lu.search.current.quick");
+        searchRequest.setParams(queryParamValueList);
+        SearchResultInfo clus = null;
+
+        try {
+            clus = getCluService().search(searchRequest, getContextInfo());
+            for (SearchResultRowInfo result : clus.getRows()) {
+                List<SearchResultCellInfo> cells = result.getCells();
+                KrmsSuggestDisplay display = new KrmsSuggestDisplay();
+                for (SearchResultCellInfo cell : cells) {
+                    if ("lu.resultColumn.cluId".equals(cell.getKey())) {
+                        display.setId(cell.getValue());
+                    } else if ("lu.resultColumn.luOptionalCode".equals(cell.getKey())) {
+                        display.setDisplayName(cell.getValue());
+                    }
+                }
+                displays.add(display);
+            }
+        } catch (Exception e) {
+            //do nothing
+        }
+
+        return displays;
+    }
+
+    public List<KrmsSuggestDisplay> getOrgDepartmentForSuggest(String orgName) {
+
+        List<KrmsSuggestDisplay> displays = new ArrayList<KrmsSuggestDisplay>();
+        List<SearchParamInfo> queryParamValueList = new ArrayList<SearchParamInfo>();
+        SearchParamInfo orgNameParam = new SearchParamInfo();
+        orgNameParam.setKey("org.queryParam.orgOptionalLongName");
+        orgNameParam.getValues().add(orgName);
+        queryParamValueList.add(orgNameParam);
+        SearchParamInfo orgTypeParam = new SearchParamInfo();
+        orgTypeParam.setKey("org.queryParam.orgOptionalType");
+        List<String> orgTypeValues = new ArrayList<String>();
+        orgTypeValues.add("kuali.org.Department");
+        orgTypeParam.setValues(orgTypeValues);
+        queryParamValueList.add(orgTypeParam);
+        SearchRequestInfo searchRequest = new SearchRequestInfo();
+        searchRequest.setSearchKey("org.search.generic");
+        searchRequest.setParams(queryParamValueList);
+        SearchResultInfo orgs = null;
+
+        try {
+            orgs = getOrganizationService().search(searchRequest, getContextInfo());
+            for (SearchResultRowInfo result : orgs.getRows()) {
+                List<SearchResultCellInfo> cells = result.getCells();
+                KrmsSuggestDisplay display = new KrmsSuggestDisplay();
+                for (SearchResultCellInfo cell : cells) {
+                    if ("org.resultColumn.orgId".equals(cell.getKey())) {
+                        display.setId(cell.getValue());
+                    } else if ("org.resultColumn.orgOptionalLongName".equals(cell.getKey())) {
+                        display.setDisplayName(cell.getValue());
+                    }
+                }
+                displays.add(display);
+            }
+        } catch (Exception e) {
+            //do nothing
+        }
+
+        return displays;
+    }
+
+    public List<KrmsSuggestDisplay> getTestNamesForSuggest(String testName) {
+
+        List<KrmsSuggestDisplay> displays = new ArrayList<KrmsSuggestDisplay>();
+        List<SearchParamInfo> queryParamValueList = new ArrayList<SearchParamInfo>();
+        SearchParamInfo testNameParam = new SearchParamInfo();
+        testNameParam.setKey("cluset.queryParam.optionalName");
+        testNameParam.getValues().add(testName);
+        queryParamValueList.add(testNameParam);
+        SearchParamInfo reusableParam = new SearchParamInfo();
+        reusableParam.setKey("cluset.queryParam.optionalReusable");
+        reusableParam.getValues().add(Boolean.TRUE.toString());
+        queryParamValueList.add(reusableParam);
+        SearchParamInfo cluSetTypeParam = new SearchParamInfo();
+        cluSetTypeParam.setKey("cluset.queryParam.optionalType");
+        cluSetTypeParam.getValues().add("kuali.cluSet.type.Test");
+        queryParamValueList.add(cluSetTypeParam);
+
+        SearchRequestInfo searchRequest = new SearchRequestInfo();
+        searchRequest.setSearchKey("cluset.search.generic");
+        searchRequest.setParams(queryParamValueList);
+        SearchResultInfo clus = null;
+
+        try {
+            clus = getCluService().search(searchRequest, getContextInfo());
+            for (SearchResultRowInfo result : clus.getRows()) {
+                List<SearchResultCellInfo> cells = result.getCells();
+                KrmsSuggestDisplay display = new KrmsSuggestDisplay();
+                for (SearchResultCellInfo cell : cells) {
+                    if ("cluset.resultColumn.cluSetId".equals(cell.getKey())) {
+                        display.setId(cell.getValue());
+                    } else if ("cluset.resultColumn.name".equals(cell.getKey())) {
+                        display.setDisplayName(cell.getValue());
+                    }
+                }
+                displays.add(display);
+            }
+        } catch (Exception e) {
+            //do nothing
+        }
+        return displays;
+    }
+    public List<KrmsSuggestDisplay> getCourseSetForSuggest(String cluSetName) {
+        List<KrmsSuggestDisplay> displays = new ArrayList<KrmsSuggestDisplay>();
+        List<SearchParamInfo> queryParamValueList = new ArrayList<SearchParamInfo>();
+        SearchParamInfo cluSetParam = new SearchParamInfo();
+        cluSetParam.setKey("cluset.queryParam.optionalName");
+        cluSetParam.getValues().add(cluSetName);
+        queryParamValueList.add(cluSetParam);
+        SearchParamInfo reusableCluSet = new SearchParamInfo();
+        reusableCluSet.setKey("cluset.queryParam.optionalReusable");
+        reusableCluSet.getValues().add(Boolean.TRUE.toString());
+        queryParamValueList.add(reusableCluSet);
+        SearchRequestInfo searchRequest = new SearchRequestInfo();
+        searchRequest.setSearchKey("cluset.search.generic");
+        searchRequest.setParams(queryParamValueList);
+        SearchParamInfo cluSetTypeParam = new SearchParamInfo();
+        cluSetTypeParam.setKey("cluset.queryParam.optionalType");
+        cluSetTypeParam.getValues().add("kuali.cluSet.type.CreditCourse");
+        queryParamValueList.add(cluSetTypeParam);
+        SearchResultInfo clus = null;
+        try {
+            clus = getCluService().search(searchRequest, getContextInfo());
+            for (SearchResultRowInfo result : clus.getRows()) {
+                List<SearchResultCellInfo> cells = result.getCells();
+                KrmsSuggestDisplay display = new KrmsSuggestDisplay();
+                for (SearchResultCellInfo cell : cells) {
+                    if ("cluset.resultColumn.cluSetId".equals(cell.getKey())) {
+                        display.setId(cell.getValue());
+                    } else if ("cluset.resultColumn.name".equals(cell.getKey())) {
+                        display.setDisplayName(cell.getValue());
+                    }
+                }
+                displays.add(display);
+            }
+        } catch (Exception e) {
+            //do nothing
+        }
+        return displays;
+    }
+    public String getTranslatedNaturalLanguage(String typeId){
+
+        NaturalLanguageUsage usage = getNaturalLanguageUsageBoService().getNaturalLanguageUsageByName(PermissionServiceConstants.KS_SYS_NAMESPACE, KsKrmsConstants.KRMS_NL_TYPE_DESCRIPTION);
+
+        NaturalLanguageTemplate template = null;
+        try{
+            template = getNaturalLanguageTemplateBoService().findNaturalLanguageTemplateByLanguageCodeTypeIdAndNluId("en", typeId, usage.getId());
+        }catch (IndexOutOfBoundsException e){
+            //Ignore, rice error in NaturalLanguageTemplateBoServiceImpl line l
+        }
+
+        //TODO: Do translation.
+
+        return template.getTemplate();
+    }
+
+    private CluService getCluService() {
+        if (cluService == null) {
+            cluService = (CluService) GlobalResourceLoader.getService(new QName(CluServiceConstants.CLU_NAMESPACE, CluServiceConstants.SERVICE_NAME_LOCAL_PART));
+        }
+        return cluService;
+    }
+
+    private ContextInfo getContextInfo() {
+        if (null == contextInfo) {
+            contextInfo = ContextUtils.createDefaultContextInfo();
+        }
+        return contextInfo;
+    }
+
+    protected OrganizationService getOrganizationService(){
+        if(organizationService == null) {
+            organizationService = (OrganizationService) GlobalResourceLoader.getService(new QName(OrganizationServiceConstants.NAMESPACE, OrganizationServiceConstants.SERVICE_NAME_LOCAL_PART));
+        }
+        return organizationService;
+    }
+
+    private NaturalLanguageUsageBoService getNaturalLanguageUsageBoService() {
+        return KsKrmsRepositoryServiceLocator.getNaturalLanguageUsageBoService();
+    }
+
+    private NaturalLanguageTemplateBoService getNaturalLanguageTemplateBoService() {
+        return KsKrmsRepositoryServiceLocator.getNaturalLanguageTemplateBoService();
+    }
+
+}
