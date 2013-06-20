da023b94b703d7746dd02ac23593103fc0bb798d
jcaddel <jcaddel@kuali.org>
2013-06-20 15:33:50 -0400

    tmp area
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-enroll/tags/builds/ks-enroll-1.0/1.0.0-M6/build-165@50184 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-enroll-ui/src/main/java/org/kuali/student/enrollment/class2/courseoffering/service/ManageSOCViewHelperService.java
@@ -0,0 +1,24 @@
+package org.kuali.student.enrollment.class2.courseoffering.service;
+
+import org.kuali.rice.krad.uif.service.ViewHelperService;
+import org.kuali.student.enrollment.acal.dto.TermInfo;
+import org.kuali.student.enrollment.class2.courseoffering.form.ManageSOCForm;
+
+import java.util.List;
+
+public interface ManageSOCViewHelperService extends ViewHelperService{
+
+    public List<TermInfo> getTermByCode(String termCode) throws Exception;
+
+    public void buildModel(ManageSOCForm socForm);
+
+    public void lockSOC(ManageSOCForm socForm);
+
+    public void allowSOCFinalEdit(ManageSOCForm socForm);
+
+    public void publishSOC(ManageSOCForm socForm);
+
+    public void closeSOC(ManageSOCForm socForm);
+
+    public void startMassScheduling(ManageSOCForm socForm);
+}
