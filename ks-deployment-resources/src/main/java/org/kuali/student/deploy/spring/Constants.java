b7a974c7401ce190c9bbd3baebe7695e6591065e
jcaddel <jcaddel@kuali.org>
2013-07-04 06:51:00 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-kuali-impex/ks-deployments/trunk@51064 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-deployment-resources/src/main/java/org/kuali/student/deploy/spring/Constants.java
@@ -0,0 +1,9 @@
+package org.kuali.student.deploy.spring;
+
+public abstract class Constants {
+
+	public static final String GROUP_ID = "org.kuali.student.deployments";
+	public static final String ARTIFACT_ID = "ks-deployment-resources";
+	public static final String KS_DB_GROUP_ID = "org.kuali.student.db";
+
+}
